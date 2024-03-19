31fbc5d32341641271fa33fa4ca8ba41bd9d2c95
NubieMe <fahmi.mellow@gmail.com>
2024-03-19 19:22:39 +0700

    update: change quiz model

--- entities/quiz.go
+++ entities/quiz.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Quiz struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Question    string             `bson:"question" json:"question"`
-	AnswerTrue  string             `bson:"answer_true" json:"answer_true"`
-	AnswerFalse []string           `bson:"answer_false" json:"answer_false"`
-	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
-	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Question  string             `bson:"question" json:"question"`
+	Answer    string             `bson:"answer" json:"answer"`
+	Option    []string           `bson:"option" json:"option"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 }
